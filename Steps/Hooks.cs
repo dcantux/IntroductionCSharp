20b8a05116bc45cb39d451239b45ef2078415b8f
Cantux <damian.cantu@gmail.com>
2024-10-23 16:37:43 -0300

    Initial Commit

---
+++ Steps/Hooks.cs
@@ -0,0 +1,14 @@
+﻿using TechTalk.SpecFlow;
+
+namespace IntroductionCSharp.StepDefinitions
+{
+    [Binding]
+    public class Hooks
+    {
+        [BeforeScenario]
+        public void SetupTest(FeatureContext featureContext)
+        {
+            Console.WriteLine("Starting " + featureContext.FeatureInfo.Title);
+        }
+    }
+}
